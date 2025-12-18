dfc075bc77ed93969e0b6f0b3d86d66f2f421d89
Prexidazie Morara <moraraprexidazie@gmail.com>
2025-12-19 00:04:01 +0300

    completed lab js-analyzing-data-modules-syntax

--- index.js
+++ index.js
@@ -4,3 +4,19 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
+function combineUsers(...userArrays) {
+  const users = [...userArrays.flat()];
+
+  const merge_date = new Date().toLocaleDateString();
+
+  return {
+    users,
+    merge_date
+  };
+}
+
+module.exports = {
+  combineUsers
+};
